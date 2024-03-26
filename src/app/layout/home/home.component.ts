29b270d44c9b2e389c561cdf71c416de191efa9d
Bút <cloly031128@gmail.com>
2024-03-26 23:59:13 +0700

    home-detail

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -4,11 +4,12 @@ import { HomeAboutComponent } from './home-about/home-about.component'
 import { HomeProjectComponent } from './home-project/home-project.component'
 import { HomeEstateRentComponent } from './home-estate-rent/home-estate-rent.component'
 import { HomeEstateSaleComponent } from './home-estate-sale/home-estate-sale.component'
+import { HomeDetailComponent } from './home-detail/home-detail.component'
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HomeSearchFilterComponent, HomeAboutComponent, HomeProjectComponent, HomeEstateRentComponent, HomeEstateSaleComponent],
+    imports: [HomeSearchFilterComponent, HomeAboutComponent, HomeProjectComponent, HomeEstateRentComponent, HomeEstateSaleComponent, HomeDetailComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
