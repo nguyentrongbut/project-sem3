dde03012aaa8168f0974361701c32223602d8c60
Bút <cloly031128@gmail.com>
2024-03-26 13:41:33 +0700

    home project

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeSearchFilterComponent } from './home-search-filter/home-search-filter.component';
 import { HomeAboutComponent } from './home-about/home-about.component';
+import { HomeProjectComponent } from './home-project/home-project.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeSearchFilterComponent, HomeAboutComponent],
+  imports: [HomeSearchFilterComponent, HomeAboutComponent, HomeProjectComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
